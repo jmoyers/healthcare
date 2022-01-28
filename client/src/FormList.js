6fc09af17c05e9952a99b93c2ff7008283795c64
Joshua Moyers <jmoyers@gmail.com>
2022-01-28 09:51:26 -0800

    Add some mobile stylings to FormList
    * Create hamburger menu for the controls on each form
    * Transition open on click of row item
    * WIP, FormList & Header both incomplete for mobile

--- client/src/FormList.js
+++ client/src/FormList.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useForms, useFormDelete, useFormCreate } from "./hooks/form";
 import { useNavigate } from "react-router-dom";
 import { nanoid } from "nanoid";
@@ -7,12 +8,15 @@ import style from "./FormList.module.scss";
 import cx from "classnames";
 
 import Button from "./Button";
+import HamburgerMenu from "./HamburgerMenu";
 
 const FormList = () => {
   const { status, data: forms } = useForms();
   const navigate = useNavigate();
   const { mutate: deleteForm } = useFormDelete();
   const { mutate: createForm } = useFormCreate();
+
+  const [activeMenu, setActiveMenu] = useState(false);
 
   const onLinkClick = async (id) => {
     await copy(
@@ -45,6 +49,30 @@ const FormList = () => {
     );
   };
 
+  const onPreviewClick = (id) => {
+    navigate(`/preview/${id}`);
+  };
+
+  const onFormMenuClick = (item, formId) => {
+    if (item === "Link") {
+      onLinkClick(formId);
+    } else if (item === "Preview") {
+      onPreviewClick(formId);
+    } else if (item === "Edit") {
+      onEditClick(formId);
+    } else if (item === "Delete") {
+      onDeleteClick(formId);
+    }
+  };
+
+  const onRowClick = (id) => {
+    if (activeMenu === id) {
+      setActiveMenu(false);
+    } else {
+      setActiveMenu(id);
+    }
+  };
+
   return (
     <div className={style.container}>
       <h1 className={style.componentTitle}>Forms</h1>
@@ -62,36 +90,41 @@ const FormList = () => {
 
         {status === "success" &&
           forms.map((form) => (
-            <div className={style.row} key={form.id}>
-              <div className={cx(style.cell, style.title)}>
-                <i className={cx(style.formIcon, "icon-folder")}></i>
-                {form.title}
+            <div key={form.id}>
+              <div className={style.row} onClick={() => onRowClick(form.id)}>
+                <div className={cx(style.cell, style.title)}>
+                  <i className={cx(style.formIcon, "icon-folder")}></i>
+                  {form.title}
+                </div>
+                <div className={cx(style.cell, style.actions)}>
+                  <Button icon="users" onClick={() => onLinkClick(form.id)}>
+                    Link
+                  </Button>
+                  <Button icon="list" onClick={() => onPreviewClick(form.id)}>
+                    Preview
+                  </Button>
+                  <Button
+                    icon="cog"
+                    type="secondary"
+                    onClick={() => onEditClick(form.id)}
+                  >
+                    Edit
+                  </Button>
+                  <Button
+                    icon="trash"
+                    type="danger"
+                    onClick={() => onDeleteClick(form.id)}
+                  >
+                    Delete
+                  </Button>
+                </div>
               </div>
-              <div className={cx(style.cell, style.actions)}>
-                <Button icon="users" onClick={() => onLinkClick(form.id)}>
-                  Link
-                </Button>
-                <Button
-                  icon="list"
-                  onClick={() => navigate(`/preview/${form.id}`)}
-                >
-                  Preview
-                </Button>
-                <Button
-                  icon="cog"
-                  type="secondary"
-                  onClick={() => onEditClick(form.id)}
-                >
-                  Edit
-                </Button>
-                <Button
-                  icon="trash"
-                  type="danger"
-                  onClick={() => onDeleteClick(form.id)}
-                >
-                  Delete
-                </Button>
-              </div>
+              <HamburgerMenu
+                open={activeMenu === form.id}
+                items={["Link", "Preview", "Edit", "Delete"]}
+                formId={form.id}
+                onClick={onFormMenuClick}
+              />
             </div>
           ))}
       </div>
--- client/src/HamburgerMenu.js
+++ client/src/HamburgerMenu.js
@@ -2,18 +2,32 @@ import style from "./HamburgerMenu.module.scss";
 import cx from "classnames";
 
 const HamburgerMenu = (props) => {
-  const { items, open, onClick = () => {} } = props;
+  const { items, open, formId, onClick = () => {} } = props;
 
   return (
     <div
       className={cx({
         [style.menu]: true,
-        [style.menuClosed]: !open,
+        [style.menuOpen]: open,
       })}
     >
       {items.map((item) => (
-        <div className={style.menuItem} onClick={() => onClick(item)}>
-          {item}
+        <div
+          key={item}
+          className={style.menuItem}
+          onClick={() => onClick(item, formId)}
+        >
+          <div
+            className={cx({
+              [style.navText]: true,
+              "icon-users": item === "Link",
+              "icon-list": item === "Preview",
+              "icon-cog": item === "Edit",
+              "icon-trash": item === "Delete",
+            })}
+          >
+            {item}
+          </div>
         </div>
       ))}
     </div>
